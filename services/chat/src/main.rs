a2242723827eb337f8e426e9a71986ed51fdc4ab
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-09-02 08:55:49 +0200

    feat(chat): adds simple http server

--- services/chat/src/main.rs
+++ services/chat/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    axum::Server::bind(&"0.0.0.0:4002".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
