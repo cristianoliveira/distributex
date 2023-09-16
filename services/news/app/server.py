df62b98862bc7e713ef7b18e5149923ea49488d5
Cristian Oliveira <contato@cristianoliveira.com.br>
2023-09-16 16:52:41 +0200

    feat: allow favorite article

--- services/news/app/server.py
+++ services/news/app/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 from app.api import fetch_top_news, fetch_details_for_story
 
 server = Flask(__name__)
@@ -17,3 +17,16 @@ def story_link(item_id):
 def story_details(item_id):
     details = fetch_details_for_story(item_id)
     return render_template("_story_details.html", details=details)
+
+favorites = dict()
+@server.route("/news/story/<item_id>/favorite")
+def story_favorite(item_id):
+    favorites[item_id] = not favorites.get(item_id, False)
+
+    favorited = favorites[item_id]
+
+    resp = make_response(render_template("_story_favorite.html", favorited=favorited))
+
+    resp.headers['HX-Trigger'] = 'news-favorited' if favorited else 'news-unfavorited'
+
+    return resp
